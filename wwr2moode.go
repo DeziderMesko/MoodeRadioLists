67c507f314db5ad8f262c3172b1bedb86c9d4cbe
Dezo Mesko <dmesko@gmail.com>
2021-04-23 21:18:51 +0200

    bah

--- wwr2moode.go
+++ wwr2moode.go
@@ -5,18 +5,41 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"time"
+	"strings"
+
+	"github.com/disintegration/imaging"
 )
 
 func main() {
-	country := "cz"
-	list := loadWWRadio(fmt.Sprintf("./ww-radio/content/stations/%s.json", country), country)
-	writeMoodeRadio("moode-output", list)
+	if len(os.Args) != 2 {
+		fmt.Println("Use country code as a parameter")
+	}
+	countryCode := os.Args[1]
+
+	if countryCode == "all" {
+		files, err := ioutil.ReadDir("./ww-radio/content/stations/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, f := range files {
+			countryCode = strings.Replace(f.Name(), ".json", "", -1)
+			fmt.Println()
+			fmt.Println(countryCode)
+			list := loadWWRadio(fmt.Sprintf("./ww-radio/content/stations/%s.json", countryCode), countryCode)
+			writeMoodeRadio(countryCode+"-output", countryCode, list)
+		}
+	} else {
+		list := loadWWRadio(fmt.Sprintf("./ww-radio/content/stations/%s.json", countryCode), countryCode)
+		writeMoodeRadio(countryCode+"-output", countryCode, list)
+	}
 }
 
 type WWRadio struct {
@@ -73,7 +96,7 @@ func (r *MoodeRadio) Marshal() ([]byte, error) {
 	return json.MarshalIndent(r, "", "    ")
 }
 
-func writeMoodeRadio(dir string, wwRadioList WWRadioList) {
+func writeMoodeRadio(outputDir, countryCode string, wwRadioList WWRadioList) {
 	var mr = &MoodeRadio{
 		Fields: []string{
 			"id",
@@ -94,10 +117,9 @@ func writeMoodeRadio(dir string, wwRadioList WWRadioList) {
 		},
 		Stations: []Station{},
 	}
-	os.Mkdir(dir, os.ModeDir)
-	logoDir := filepath.Join(dir, "radio-logos")
-	os.Mkdir(logoDir, os.ModeDir)
-	os.Mkdir(filepath.Join(logoDir, "thumbs"), os.ModeDir)
+
+	os.Mkdir(outputDir, os.ModeDir)
+	rootDir, _ := os.Getwd()
 
 	for idx, radio := range wwRadioList.Country {
 
@@ -107,34 +129,44 @@ func writeMoodeRadio(dir string, wwRadioList WWRadioList) {
 			HomePage: radio.SiteURL,
 			Station:  radio.RadioURL,
 			Logo:     "local",
+			Type:     "r",
 		}
 
 		mr.Stations = append(mr.Stations, station)
-		err := saveLogo(logoDir, radio.Image, radio.Name)
+		err := saveLogo(outputDir, radio.Image, countryCode, radio.Name)
 		if err != nil {
-			fmt.Println("Error saving radio name: ", radio.Name, err)
+			fmt.Println("Error saving radio logo: ", radio.Name, err)
 		}
-		time.Sleep(50)
 	}
 
 	result, err := mr.Marshal()
 	if err != nil {
 		fmt.Print(err)
 	}
-
-	ioutil.WriteFile(filepath.Join(dir, "station_data.json"), result, os.ModePerm)
+	os.Chdir(rootDir)
+	ioutil.WriteFile(filepath.Join(outputDir, "station_data.json"), result, os.ModePerm)
 
 }
 
-func saveLogo(logoDir, wwrName, MoodeName string) error {
-	imagesURL := "https://www.s3blog.org/s3radio-files/stations/cz/"
+func saveLogo(targetDir, wwrName, countryCode, MoodeName string) error {
+	if countryCode == "us_2" {
+		countryCode = "us"
+	}
+	imagesURL := fmt.Sprintf("https://www.s3blog.org/s3radio-files/stations/%s/", countryCode)
+	jpegFileName := MoodeName + ".jpg"
+	logoDir := filepath.Join(targetDir, "radio-logos")
+
+	os.Mkdir(logoDir, os.ModeDir)
+
 	err := os.Chdir(logoDir)
 
-	if Exists(MoodeName + ".png") {
+	// dont download if you have it
+	if FileExists(jpegFileName) {
 		fmt.Print("x")
 		return nil
 	}
 
+	// download
 	fmt.Print(".")
 	response, err := http.Get(imagesURL + wwrName)
 	if err != nil {
@@ -145,24 +177,41 @@ func saveLogo(logoDir, wwrName, MoodeName string) error {
 	if response.StatusCode != 200 {
 		return errors.New("Received non 200 response code")
 	}
-	//Create a empty file
-	file, err := os.Create(MoodeName + ".png")
+
+	//convert png to jpeg
+	pngImage, _ := png.Decode(response.Body)
+
+	scaledImage := imaging.Resize(pngImage, 400, 0, imaging.Lanczos)
+
+	file, err := os.Create(jpegFileName)
+	jpeg.Encode(file, scaledImage, nil)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	//Write the bytes to the fiel
+	// save file
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	// copy to thumbnails
+	input, err := ioutil.ReadFile(jpegFileName)
+	if err != nil {
+		return err
+	}
 
+	os.Mkdir("thumbs", os.ModeDir)
+	err = ioutil.WriteFile(filepath.Join("thumbs", MoodeName+"_sm"+".jpg"), input, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func Exists(name string) bool {
+func FileExists(name string) bool {
 	if _, err := os.Stat(name); err != nil {
 		if os.IsNotExist(err) {
 			return false
